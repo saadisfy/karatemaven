05bc8f02c69c7bd59be92af0c2b1836adef0da23
Saad Masood <saadmasood@web.de>
2023-05-30 01:40:45 +0200

    adjust main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
 	"os"
 
+	"github.com/pkg/errors"
+
+	"github.com/kubeshop/testkube/pkg/envs"
 	"github.com/kubeshop/testkube/pkg/executor/agent"
+	"github.com/kubeshop/testkube/pkg/executor/output"
+
 	"github.com/saadisfy/karatemaven/pkg/runner"
 )
 
 func main() {
-	agent.Run(runner.NewRunner(), os.Args)
+	contextBackground := context.Background()
+	params, err := envs.LoadTestkubeVariables()
+	if err != nil {
+		output.PrintError(os.Stderr, errors.Errorf("could not initialize Karate Executor environment variables: %v", err))
+		os.Exit(1)
+	}
+	karateRunner, err := runner.NewKarateRunner(contextBackground, os.Getenv("DEPENDENCY_MANAGER"), params)
+	if err != nil {
+		output.PrintError(os.Stderr, errors.Wrap(err, "Error instantiating Karate Executor"))
+		os.Exit(1)
+	}
+	agent.Run(contextBackground, karateRunner, os.Args)
 }
